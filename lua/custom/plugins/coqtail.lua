c1782e0e0e36bfc9fb3ac338f14427e416ebf467
Loïc <lpeyrot@irif.fr>
2024-07-24 18:14:59 +0200

    SMT-Coq dans Coqtail et ne pas formatter tex

--- lua/custom/plugins/coqtail.lua
+++ lua/custom/plugins/coqtail.lua
@@ -2,6 +2,7 @@ return {
   'whonore/Coqtail',
   config = function()
     vim.g.coqtail_noimap = true
+    vim.g.coqtail_treat_stderr_as_warning = 1 -- For SMT-Coq
     -- vim.g.coqtail_map_prefix = 't'
     vim.keymap.set('', 'à', '<Plug>CoqNext')
     vim.keymap.set('', 'À', '<Plug>CoqUndo')
