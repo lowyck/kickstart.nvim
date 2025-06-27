0fa87d26b9498be4d0d0052cbebfda79e2a23171
Loïc <lpeyrot@irif.fr>
2025-06-27 21:22:32 +0200

    texpresso plugin

--- lua/custom/plugins/texpresso.lua
+++ lua/custom/plugins/texpresso.lua
@@ -1,6 +1,6 @@
 return {
-  'let-def/texpresso.vim',
+  'let-def/texpresso.nvim',
   config = function()
-    require('texpresso').texpresso_path = '/home/lpeyrot/System/texpresso/build/texpresso'
+    require('texpresso').texpresso_path = '/home/eemency/System/texpresso/build/texpresso'
   end,
 }
